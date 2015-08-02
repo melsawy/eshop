231b459f83257e7da2f8a2ac86ac9822401195ec
Sawy <melsawy.be7eri@gmail.com>
2015-08-02 23:57:18 +0200

    Ticket #1090

--- sites/all/modules/custom/ringo_eshop/js/eshop_checkout.js
+++ sites/all/modules/custom/ringo_eshop/js/eshop_checkout.js
@@ -1,8 +1,8 @@
 Drupal.behaviors.eshopCheckout = {
     attach : function(context) {
-        jQuery("table.eshop-checkout-sroll-table td.checkout-scroll-tab").click(function() {
-            targetId = jQuery(this).attr("data-target") ; 
-            jQuery("html, body").animate({scrollTop: jQuery('#' + targetId).offset().top });
+        jQuery('table.eshop-checkout-sroll-table td.checkout-scroll-tab').click(function() {
+            targetId = jQuery(this).attr('data-target') ; 
+            jQuery('html, body').animate({scrollTop: jQuery('#' + targetId).offset().top });
         });   
     }
 }
