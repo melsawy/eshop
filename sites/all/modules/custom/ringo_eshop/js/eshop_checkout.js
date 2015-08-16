c26c4910cdc1f1282e28c5ff81bf432a90101042
Sawy <melsawy.be7eri@gmail.com>
2015-08-16 20:47:51 +0200

    Ticket #1090

--- sites/all/modules/custom/ringo_eshop/js/eshop_checkout.js
+++ sites/all/modules/custom/ringo_eshop/js/eshop_checkout.js
@@ -1,7 +1,5 @@
 
 jQuery(document).ready(function () {
-    jQuery('table.eshop-checkout-sroll-table td.checkout-scroll-tab').unbind('click');
-    jQuery('table.eshop-checkout-sroll-table td.checkout-scroll-tab').bind('click');
     jQuery('table.eshop-checkout-sroll-table td.checkout-scroll-tab').click(function() {
             targetId = jQuery(this).attr('data-target') ; 
             jQuery('html, body').animate({scrollTop: jQuery('#' + targetId).offset().top - 110});
